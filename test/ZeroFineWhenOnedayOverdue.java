560c6d8257a088c1be15503127afba20a739e232
Gaury <chethana9804@gmail.com>
2021-10-16 22:32:34 +0530

    ädded junit test codes to reproduce errors

---
+++ test/ZeroFineWhenOnedayOverdue.java
@@ -0,0 +1,50 @@
+import java.util.Date;
+import library.entities.Calendar;
+import library.entities.ICalendar;
+import library.entities.ILibrary;
+import library.entities.ILoan;
+import library.entities.helpers.BookHelper;
+import library.entities.helpers.CalendarFileHelper;
+import library.entities.helpers.LibraryFileHelper;
+import library.entities.helpers.LoanHelper;
+import library.entities.helpers.PatronHelper;
+import org.junit.Before;
+import org.junit.Test;
+import static junit.framework.Assert.assertEquals;
+
+/**
+ *
+ * @author Gauri..Chethana
+ */
+public class ZeroFineWhenOnedayOverdue {
+
+    private static LibraryFileHelper libraryHelper;
+    private static ILibrary library;
+    private ILoan currentLoan;
+    static final double FINE_PER_DAY = 1.0;
+    private static ICalendar calendar;
+    private static CalendarFileHelper calendarHelper;
+
+    @Before
+    public void setUp() {
+        calendarHelper = new CalendarFileHelper();
+        calendar = calendarHelper.loadCalendar();
+        libraryHelper = new LibraryFileHelper(new BookHelper(), new PatronHelper(), new LoanHelper());
+        library = libraryHelper.loadLibrary();
+        currentLoan = library.getCurrentLoanByBookId(1);
+    }
+
+    @Test
+    public void calculateOverDueFineByOneDay() {
+        double fine = 0.0;
+        if (currentLoan.isOverDue()) {
+            Date dueDate = currentLoan.getDueDate();
+            calendar.setDate(dueDate);
+            calendar.incrementDate(1);
+            long daysOverDue = Calendar.getInstance().getDaysDifference(dueDate);
+            fine = daysOverDue * FINE_PER_DAY;
+            assertEquals(1.0, fine);
+        }
+
+    }
+}
